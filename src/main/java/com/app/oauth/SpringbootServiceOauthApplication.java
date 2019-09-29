38f1e526eed68e57c811c652fb5bf0c62297ba10
Jocs <jhassan8@outlook.com>
2019-09-28 22:35:43 -0300

    test code

--- src/main/java/com/app/oauth/SpringbootServiceOauthApplication.java
+++ src/main/java/com/app/oauth/SpringbootServiceOauthApplication.java
@@ -6,12 +6,32 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableEurekaClient
-@SpringBootApplication
 @EnableFeignClients
-public class SpringbootServiceOauthApplication {
-
+@SpringBootApplication
+public class SpringbootServiceOauthApplication/* implements CommandLineRunner*/ {
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootServiceOauthApplication.class, args);
 	}
+
+	
+	/**
+	 * this comment block and the implements is used for 
+	 * create a random encrypted password for testing
+	 */
+	
+	/*
+	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		for (int i = 0; i < 4; i++) {
+			System.out.println(bCryptPasswordEncoder.encode("12345"));
+		}
+	}
+	
+	*/
 
 }
